9d24fb4d450fb1de9b5a8240e79b9475200891e0
DARONASS666 <daron_1997@hotmail.com>
2021-05-23 21:10:45 +0400

    Python Package for https://bit.ly/intro_nlp

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+# open the readme as set the description
+# for the setup.py file as the description
+# in the README file
+with open('README.md' , 'r') as file:
+	long_description = file.read()
+
+setuptools.setup(
+	name = 'preprocess_dassadourian', #this should be unique
+	version = '0.0.1',
+	author = 'Daron Assadourian',
+	author_email = 'daron_1997@hotmail.com',
+	description = 'This is a preprocessing package',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Approved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
